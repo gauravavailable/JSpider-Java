068d7ed0fc179aebd270ec51d6db8ec8bed7f61b
gauravavailable <gk0215312@gmail.com>
2025-11-14 13:50:59 +0530

    jhvcjhbjb

--- src/com/NumberLopps/Prime.java
+++ src/com/NumberLopps/Prime.java
@@ -3,5 +3,6 @@ package com.NumberLopps;
 public class Prime {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("vhsavh");
     }
 }
